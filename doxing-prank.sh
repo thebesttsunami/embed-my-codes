8715515147b075dc9a776ed58cfe0a7b2f61bbb2
Parhan Tyz <162686236+thebesttsunami@users.noreply.github.com>
2024-12-15 16:39:37 +0700

    Create doxing-prank.sh

---
+++ doxing-prank.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#[created by mr. daffa kibodey]#
+#[created 23: 55 - 12/12/2024]#
+
+echo "[uploading...] DATA BASE - 528008 [[SMPN 101 JAKARTA]]"
+        echo "NAMA: RISKY BAFANDA"
+      echo "USIA: 15 thn"
+    echo "KELAS: 8.5 (2 SMP)"
+  echo "IBU: Dr. Wulandari Sp,d S.sos"
+    echo "AYAH: Dr. Risky Farizal S.H M.Hum"
+      echo "NIS: 52759"
+        echo "NISN: 52755005"
+           echo "TLP: 6285194996351"
+            echo "ALAMAT: Penjaringan Jakata Utara, Indonesia"
+          echo "[[valid - 528008 database...]]"
